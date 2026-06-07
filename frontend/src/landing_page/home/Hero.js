bc75aafde50b08f417ba7c369993d4afd806fd4a
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-06-07 12:48:46 +0530

    updated

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,7 +3,14 @@ import React from "react";
 function Hero() {
   return (
     
-      <h1>Hero <i class="fa-solid fa-circle-user"></i></h1>
+     <div className="container p-5">
+       <div className="row text-center">
+      <img src="media/images/homeHero.png" alt="Hero Image" className="mb-5"/>
+          <h1 className="mt-5">Invest in everything</h1>
+         <p>Discover the power of investing with our platform</p>
+        <button className="p-3 btn btn-primary fs-5" style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
+       </div>
+     </div>
      
   );
 }

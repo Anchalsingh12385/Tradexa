944af9565d14396ce9b7eb3c5be427af3a8d0ec2
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-06-11 11:58:22 +0530

    updated

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -2,13 +2,61 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav>
-      <h1>My App</h1>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#pricing">Pricing</a></li>
-        <li><a href="#open-account">Open Account</a></li>
-      </ul>
+    <nav
+      class="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          <img
+            src="media/images/tradexalogo.png"
+            style={{ width: "35%" }}
+            alt="Logo"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Product
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
     </nav>
   );
 }
